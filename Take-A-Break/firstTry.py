ce3ffb6dedd8c856ab42c11e277ed7bffe17a70a
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-02-08 00:18:06 +0330

    a program that will play your fav video when its time to take a break

---
+++ Take-A-Break/firstTry.py
@@ -0,0 +1,7 @@
+import time 
+import webbrowser
+
+
+for i in range(3):
+	time.sleep(10)
+	webbrowser.open("https://www.youtube.com/watch?v=7wtfhZwyrcc")
---
+++ Take-A-Break/openURL.py
@@ -0,0 +1,3 @@
+import webbrowser
+
+webbrowser.open("https://www.youtube.com/watch?v=7wtfhZwyrcc")
